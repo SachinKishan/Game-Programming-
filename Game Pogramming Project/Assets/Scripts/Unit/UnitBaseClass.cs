07d59728da69132675cab1add0e9de597375ebf5
SachinKishan <sachinkishan02@gmail.com>
2022-02-04 20:59:54 +0530

    set up basic character cycle

--- Game Pogramming Project/Assets/Scripts/Unit/UnitBaseClass.cs
+++ Game Pogramming Project/Assets/Scripts/Unit/UnitBaseClass.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UnitBaseClass : MonoBehaviour
 {
+	[SerializeField]int health;
+	[SerializeField]string name;
     // Start is called before the first frame update
     void Start()
     {
