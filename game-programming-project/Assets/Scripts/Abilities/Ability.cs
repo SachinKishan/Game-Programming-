fceb99168d2df1362dd0466e1e770ce4eff75c23
Aryan Sharma <aryansharma1197@gmail.com>
2022-03-13 14:40:41 +0530

    Update Ability.cs

--- game-programming-project/Assets/Scripts/Abilities/Ability.cs
+++ game-programming-project/Assets/Scripts/Abilities/Ability.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ability : ScriptableObject
 {
 	public string abilityName;
+	public float cooldownTime;
+    public float activeTime;
+
 	public virtual void Activate()
 	{
 		Debug.Log(name+ " activated");
